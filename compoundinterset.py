36fed750d4938335fa0fad58e81d0e316f475125
ankitshah27 <ankit05comp2022@kcc.edu.np>
2023-12-30 08:27:53 +0545

    hi

---
+++ compoundinterset.py
@@ -0,0 +1,5 @@
+principle=float(input("Enter the principle"))
+rate=float(input("Enter the rate"))
+time=float(input("enter the time"))
+interest=(principle*rate*time)/100
+print("the simple interest of given peinciple is",interest)
